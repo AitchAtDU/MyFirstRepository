453078488cbae2e63e42023648d2d538294e5df1
AitchAtDU <cxpq63@durham.ac.uk>
2020-10-12 17:49:28 +0100

    Completed the framework for objects and added method to create lists

--- LinkedListsOOP.py
+++ LinkedListsOOP.py
@@ -11,4 +11,17 @@ class ListNode():
     def __str__(self):
         return self.Data
 
+class LinkedList():
+    def __init__(self,head):
+        self.Head = head
+        self.Tail = head
+        self.Size = 0
+    
+    
+
+def createLinkedList(headData):
+    headNode = ListNode(headData,None)
+    return LinkedList(headNode)
+
+
 
